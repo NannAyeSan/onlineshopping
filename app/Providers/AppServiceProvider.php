69f90641baeb54f80397b420ce1f707e88098e43
NannAyeSan <70218664+NannAyeSan@users.noreply.github.com>
2020-09-08 09:55:38 +0630

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        $categories = Category::all();
-        View::share('categories',$categories);
+        //$categories = Category::all();
+        //View::share('categories',$categories);
     }
 }
